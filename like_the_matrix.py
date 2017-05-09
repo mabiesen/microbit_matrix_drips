46ac9f3e5097aa4ce2b7ba1fafde1894c53ebcb0
Matthew Biesen <upandcomming88@gmail.com>
2017-05-09 01:18:49 -0500

    Update like_the_matrix.py

--- like_the_matrix.py
+++ like_the_matrix.py
@@ -12,32 +12,40 @@ class Drip():
     self.currenty = 0
     self.trailone = currenty - 1
     self.trailtwo = currenty - 2
-    
-  def move_trail_two(self):
+  
+  def set_all_low(self):
     display.set_pixel(self.currentx,self.trailtwo,low)
+    display.set_pixel(self.currentx,self.trailone,low)
+    display.set_pixel(self.currentx,self.currenty,low)
+    
+    
+  def increase_y_value(self):
+    self.trailtwo = self.trailtwo + 1
+    self.trailone = self.trailone + 1
+    self.currenty = self.currenty + 1
+  
+  def move_trail_two(self):
     if self.trailtwo <= 4 and self.trailtwo >= 0:
-      self.trailtwo = self.trailtwo + 1
       display.set_pixel(self.currentx,self.trailtwo,mid_high_two)
 
   def move_trail_one(self):
-    display.set_pixel(self.currentx,self.trailone,low)
     if self.trailtwo < 4 and self.trailtwo >= 0:
-      self.trailone = self.trailone + 1
       display.set_pixel(self.currentx,self.trailone,mid_high_one)
 
   def move_current(self):
     if self.trailtwo < 4 and self.trailtwo >= 0:
-      display.set_pixel(self.currentx,self.currenty,low)
-      self.currenty = self.currenty + 1
       display.set_pixel(self.currentx,self.currety,high)
     
   def move_all(self):
+    self.set_all_low()
     self.move_current()
     self.move_trailone()
     self.move_trailtwo()
     
 def main():
   myobject = Drip()
+  while myobject():
+    
   
     
 
