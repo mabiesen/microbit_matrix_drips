6a11972f8b8fd5286af66bff7b77da9fc944cbab
Matthew Biesen <upandcomming88@gmail.com>
2017-05-09 03:20:45 -0500

    Update like_the_matrix.py

--- like_the_matrix.py
+++ like_the_matrix.py
@@ -1,17 +1,19 @@
 from microbit import *
 import random
-
+####################################################################################
+####### BEGIN CLASS ################################
+# When called this class produces an object controls a single "drip" on the screen
 class Drip():
   
-  def __init__(self,startx):
+  def __init__(self,startx,starty):
     self.currentx = startx
     self.low = 0
     self.high = 9
     self.mid_high_one = 6
     self.mid_high_two = 3
-    self.currenty = 0
-    self.trailone = -1
-    self.trailtwo = -2
+    self.currenty = starty
+    self.trailone = starty-1
+    self.trailtwo = starty-2
     self.set_all_high()
     
   def set_all_high(self):
@@ -41,14 +43,44 @@ class Drip():
     self.set_all_low()
     self.increase_y_value()
     self.set_all_high()
-    
-while True:
-  startx = random.randint(0,4)
-  myobject = Drip(startx)
-  while myobject:
-    if myobject.currenty > 7:
-      del myobject
-      break
-    else:
-      myobject.move_the_drip()
-      sleep(100)
+
+############### END CLASS #######################################
+#################################################################################
+
+#################################################################
+################## Tester functions #####################################   
+
+def one_at_a_time():
+    while True:
+      startx = random.randint(0,4)
+      myobject = Drip(startx)
+      while myobject:
+        if myobject.currenty > 7:
+          del myobject
+          break
+        else:
+          myobject.move_the_drip()
+          sleep(100)
+          
+def waterfall():
+    while True:
+        first = Drip(0,0)
+        second = Drip(1,-2)
+        third = Drip(2,-4)
+        fourth = Drip(3,-6)
+        fifth = Drip(4,-8)
+        my_water = [first,second,third,fourth,fifth]
+        while True:
+            for water in my_water:
+                water.move_the_drip()
+            if my_water[-1].currenty > 7:
+                break
+            sleep(300)
+        
+      
+          
+######################## END TESTER FUNCTIONS ######################################
+#################################################################################
+
+# kick off program
+waterfall()
